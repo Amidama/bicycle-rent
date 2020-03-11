b32e95fb686117fab1815128440345f88e0aa611
Amidamaru <oat@gettgo.com>
2020-03-11 23:15:22 +0700

    php artisan ui vue --auth

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+      <meta name="csrf-token" content="{{ csrf_token() }}">
+
+      <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="{{ url('css/app.scss') }}">
+      <link rel="stylesheet" type="text/css" href="{{ url('css/login.scss') }}">
+      <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+			<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+			<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+      <title>Bicycle Rent - @yield('title')</title>
+    </head>
+    <body>
+      <div class="container">
+        @yield('content')
+      <div> 
+    </body>
+</html>
